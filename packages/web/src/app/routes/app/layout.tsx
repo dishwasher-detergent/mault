e98fecf9273698af99c94fdc0e8f344f3fdc3263
Kenneth Bass <kenny_bass@outlook.com>
2026-06-09 10:11:49 -0500

    add org to footer

--- packages/web/src/app/routes/app/layout.tsx
+++ packages/web/src/app/routes/app/layout.tsx
@@ -6,6 +6,7 @@ import { RequireCollectionDialog } from "@/components/require-collection-dialog"
 import { StatusFooter } from "@/components/status-footer";
 import { useOrg } from "@/features/companies/api/use-organization";
 import { useIsMobile } from "@/hooks/use-is-mobile";
+import { IconPointFilled } from "@tabler/icons-react";
 import { Outlet } from "react-router-dom";
 
 export default function AppLayout() {
@@ -23,21 +24,25 @@ export default function AppLayout() {
           <AppNav />
         </div>
       ) : (
-        <div className="h-dvh w-dvw overflow-hidden p-2 pb-6 bg-muted dark:bg-black relative">
+        <div className="h-dvh w-dvw overflow-hidden p-2 pb-6 bg-muted dark:bg-black relative text-muted-foreground">
           <div className="flex flex-row bg-background border rounded-lg size-full overflow-hidden">
             <AppNav />
             <main className="flex-1 min-w-0 overflow-hidden flex flex-col">
               <Outlet />
             </main>
           </div>
-          <div className="absolute bottom-0 left-0 px-4 h-6 flex items-center w-full gap-4 text-xs">
+          <div className="absolute bottom-0 left-0 px-4 h-6 flex items-center w-full gap-2 text-xs">
+            {activeOrg && (
+              <>
+                <span>{activeOrg.name}</span>
+                <IconPointFilled className="size-2" />
+              </>
+            )}
             <AppBreadcrumb />
-            <div className="ml-auto flex items-center gap-3 shrink-0 text-muted-foreground">
+            <div className="ml-auto flex items-center gap-2 shrink-0">
               <StatusFooter />
-              <span>
-                {activeOrg && <span>{activeOrg.name} · </span>}v
-                {__APP_VERSION__}
-              </span>
+              <IconPointFilled className="size-2" />
+              <span>v{__APP_VERSION__}</span>
               <EnvBanner />
             </div>
           </div>
