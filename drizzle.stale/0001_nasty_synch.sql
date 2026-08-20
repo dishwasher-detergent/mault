69bab83c358a46ad37e6e90c9a6501141186d9ea
Kenneth Bass <kenny_bass@outlook.com>
2026-08-19 20:14:22 -0500

    remove stale migrations

--- drizzle.stale/0001_nasty_synch.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "sort_bin_presets" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"user_id" text NOT NULL,
-	"name" text NOT NULL,
-	"bins" jsonb NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL
-);
