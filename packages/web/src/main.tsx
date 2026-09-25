f5afb48e79ed004d4053aa9b4636dc4ea3c9e780
Kenneth Bass <kenny_bass@outlook.com>
2026-09-25 07:49:47 -0500

    add perfmance check for frontend

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -5,19 +5,18 @@ import "@/lib/i18n";
 import { registerServiceWorker } from "@/lib/pwa";
 import "@/lib/rollbar";
 import { ThemeProvider } from "next-themes";
-import { StrictMode, Suspense } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+performance.mark("app:modules-evaluated");
 registerServiceWorker();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <TooltipProvider>
-        <Suspense fallback={null}>
-          <RouterProvider router={router} />
-        </Suspense>
+        <RouterProvider router={router} />
       </TooltipProvider>
     </ThemeProvider>
   </StrictMode>,
